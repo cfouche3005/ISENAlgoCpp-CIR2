4812594ea2077ebfa89320671984ff44e03b3ad0
cfouche3005 <cfouche3005@gmail.com>
2022-12-06 15:38:38 +0100

    TP3

--- TP3/ex1/main.h
+++ TP3/ex1/main.h
@@ -2,7 +2,24 @@
 // Created by cfouche on 06/12/22.
 //
 
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <cmath>
+
 #ifndef ISENALGOCPPCIR2_MAIN_H
 #define ISENALGOCPPCIR2_MAIN_H
 
 #endif //ISENALGOCPPCIR2_MAIN_H
+
+class Postion {
+    int PosX;
+    int PosY;
+};
+
+class Perso{
+    char* name;
+    int PV;
+
+};
